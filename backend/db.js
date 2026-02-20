7b9ba894129170bd58849b20415227ed5cfaaecc
-Shinnamon- <chimonkung0@gmail.com>
2026-02-20 23:29:06 +0700

    chore: update default database connection URL for cloud deployment

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 const dbUrl =
-  process.env.DATABASE_URL || "postgres://user:password@localhost:5432/gymbro_db";
+  process.env.DATABASE_URL || "postgres://webadmin:OFQzlb19621@node86180-env-2210254.proen.app.ruk-com.cloud:11857/gymbro_db";
 
 const sequelize = new Sequelize(dbUrl, {
   dialect: "postgres",
